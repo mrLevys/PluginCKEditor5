11bd1479961020e4d23dda2ec340bdc13a9fdd60
levy.buarque <levy.buarque@gmail.com>
2024-09-06 17:30:59 -0300

    feat: adjustments and implementations of ckeditor5

--- src/components/CustomEditor.tsx
+++ src/components/CustomEditor.tsx
@@ -1,34 +1,51 @@
-/* eslint-disable @typescript-eslint/no-explicit-any */
-import React, { useState } from 'react';
+import { useState, useEffect, useRef } from 'react';
+import { ClassicEditor, Bold, Essentials, Italic, Link, Font, Paragraph, Underline, Undo } from 'ckeditor5';
 import { CKEditor } from '@ckeditor/ckeditor5-react';
-import ClassicEditor from '@ckeditor/ckeditor5-build-classic'
+
+import 'ckeditor5/ckeditor5.css';
 import BlockQuoteCustom from '../plugins/BlockQuoteCustom';
 
+const CustomEditor = () => {
+  const [editorData, setEditorData] = useState<string>('');
+  const editorContainerRef = useRef(null);
+	const editorRef = useRef(null);
+	const [isLayoutReady, setIsLayoutReady] = useState(false);
 
-const CustomEditor: React.FC = () => {
-    const [editorData, setEditorData] = useState<string>('');
+	useEffect(() => {
+		setIsLayoutReady(true);
 
-    return (
-        <div>
-            <h2>Editor com Citação Destacada</h2>
-            <CKEditor
-                editor={ClassicEditor}
-                data={editorData}
-                config={{
-                    plugins: [ BlockQuoteCustom, ...ClassicEditor.builtinPlugins ],
-                    toolbar: [ 'blockQuoteCustom', 'bold', 'italic', 'undo', 'redo' ]
-                }}
-                onChange={(event: any, editor: any) => {
-                    const data = editor.getData();
-                    setEditorData(data);
-                }}
-            />
-            <div>
-                <h3>Conteúdo Atual:</h3>
-                <div dangerouslySetInnerHTML={{ __html: editorData }} />
-            </div>
+		return () => setIsLayoutReady(false);
+	}, []);
+
+  return (
+    <div>
+      <h2>Editor com Citação Destacada</h2>
+      <div className="editor-container editor-container_classic-editor" ref={editorContainerRef}>
+        <div className="editor-container__editor">
+          <div ref={editorRef}>{isLayoutReady && 
+            <CKEditor 
+              editor={ClassicEditor} 
+              config={{
+                toolbar: [ 'undo', 'redo','|','bold','italic','underline','link','|', 'fontSize', 'fontFamily', 'fontColor', 'fontBackgroundColor','|','blockQuoteCustom'],
+                plugins: [ Bold,Essentials,Italic,Font,Link,Paragraph,Underline,Undo, BlockQuoteCustom ],
+                initialData: 'Show de pelota',
+                placeholder: 'Digite seu texto aqui!',
+              }}
+              // eslint-disable-next-line @typescript-eslint/no-explicit-any
+              onChange={(_event:any, editor:any) => {
+                const data = editor.getData();
+                setEditorData(data);
+              }}
+            />}
+          </div>
         </div>
-    );
+      </div>
+      <div>
+        <h3>Conteúdo Atual:</h3>
+        <div dangerouslySetInnerHTML={{ __html: editorData }} />
+      </div>
+    </div>
+  );
 };
 
 export default CustomEditor;
--- src/plugins/BlockQuoteCustom.ts
+++ src/plugins/BlockQuoteCustom.ts
@@ -1,7 +1,8 @@
-/* eslint-disable @typescript-eslint/no-unused-vars */
-import Plugin from '@ckeditor/ckeditor5-core/src/plugin';
-import ButtonView from '@ckeditor/ckeditor5-ui/src/button/buttonview';
-import { toWidget, toWidgetEditable } from '@ckeditor/ckeditor5-widget/src/utils';
+import { Plugin } from 'ckeditor5';
+import { ButtonView } from 'ckeditor5';
+import { toWidgetEditable } from 'ckeditor5';
+import {Element, Writer} from 'ckeditor5'
+
 
 export default class BlockQuoteCustom extends Plugin {
     init() {
@@ -12,22 +13,37 @@ export default class BlockQuoteCustom extends Plugin {
 
             view.set({
                 label: 'Citação destacada',
-                icon: '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 270 270" fill="#FF1123"><path d="M191.21 228.73c12.08 0 22.6-8.22 25.53-19.93l29.12-116.46c6.48-25.94-13.13-51.06-39.87-51.06-22.7 0-41.1 18.4-41.1 41.1v120.05c0 14.52 11.79 26.3 26.32 26.3zm-142 0c12.08 0 22.6-8.22 25.53-19.93l29.12-116.46c6.48-25.94-13.13-51.06-39.87-51.06-22.7 0-41.1 18.4-41.1 41.1v120.05c0 14.52 11.79 26.3 26.32 26.3z"></path></svg>', 
+                icon: '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 270 270" fill="#000000"><path d="M191.21 228.73c12.08 0 22.6-8.22 25.53-19.93l29.12-116.46c6.48-25.94-13.13-51.06-39.87-51.06-22.7 0-41.1 18.4-41.1 41.1v120.05c0 14.52 11.79 26.3 26.32 26.3zm-142 0c12.08 0 22.6-8.22 25.53-19.93l29.12-116.46c6.48-25.94-13.13-51.06-39.87-51.06-22.7 0-41.1 18.4-41.1 41.1v120.05c0 14.52 11.79 26.3 26.32 26.3z"></path></svg>', 
                 tooltip: true
             });
 
             view.on('execute', () => {
-                editor.model.change(writer => {
+                editor.model.change((writer: Writer) => {
                     const blockQuote = writer.createElement('blockQuoteCustom');
-                    editor.model.insertContent(blockQuote);
-                    writer.setSelection(blockQuote, 'on');
+                    const insertPosition = editor.model.document.selection.getFirstPosition();
+
+                    if (insertPosition) {
+                        writer.insert(blockQuote, insertPosition);
+
+                        const paragraph = writer.createElement('paragraph');
+                        writer.insert(paragraph, blockQuote);
+                        writer.setSelection( blockQuote, 'in');
+                    } else {
+                        console.error('Erro: Não foi possível determinar a posição de inserção.');
+                    }
                 });
             });
 
             return view;
         });
 
-        // Conversões para o modelo e a visualização
+        editor.model.schema.register('blockQuoteCustom', {
+            isObject: true,
+            allowWhere: '$block',
+            allowIn: '$block',
+            allowContentOf: '$block',
+        });
+
         editor.conversion.for('upcast').elementToElement({
             model: 'blockQuoteCustom',
             view: {
@@ -46,9 +62,10 @@ export default class BlockQuoteCustom extends Plugin {
 
         editor.conversion.for('editingDowncast').elementToElement({
             model: 'blockQuoteCustom',
-            view: (modelElement, { writer: viewWriter }) => {
+            view: (modelElement: Element, { writer: viewWriter }) => {
                 const blockQuote = viewWriter.createEditableElement('blockquote', {
-                    class: 'custom-blockquote'
+                    class: 'custom-blockquote',
+                    style: 'border-top: 1px solid #d1ac66; border-bottom: 1px solid #d1ac66; border-right: 1px solid #d1ac66; border-left: 15px solid #d1ac66; padding: 10px; font-style: italic;'
                 });
                 return toWidgetEditable(blockQuote, viewWriter);
             }
